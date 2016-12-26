b774d41e7e926450ac23cae9b5ab1f4d085958f6
modi7 <alouati@outlook.com>
2016-12-26 16:42:44 +0100

    Charts

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var bodyParser = require('body-parser');
+var open = require('open');
 //var express = require('express');
 
 var app = module.exports = loopback();
@@ -11,29 +12,30 @@ var app = module.exports = loopback();
 app.use(bodyParser.json());
 
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+    // start the web server
+    return app.listen(function() {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            var explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+        open(baseUrl);
+    });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+    if (err) throw err;
 
-  /*  var  publicPath = '/resources',
+    /*  var  publicPath = '/resources',
 	    year = 60 * 60 * 24 * 365 * 1000,
 	directory = __dirname + publicPath;
   app.use(publicPath, app.static(directory, { maxAge: year, allow: false }));*/
-  
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+
+    // start the server if `$ node server.js`
+    if (require.main === module)
+        app.start();
 });
