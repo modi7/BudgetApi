7c614e7e8ce68d72940a45488e27462c33b17295
modi7 <alouati@outlook.com>
2016-12-29 08:01:50 +0100

    ajout pourcentages

--- common/models/repartition-aff.js
+++ common/models/repartition-aff.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Repartitionaff) {
+//var connector = Repartitionaff.getDataSource().connector;
+	// Repartitionaff.observe('loaded', function logQuery(ctx, next) {
+
+    //         try {
+    //         var fTotal = ctx.instance.__data.reduce(function(first,second){
+    //         return first + second; }, 0);
+    //         ctx.instance.__data.forEach(function(item){
+    //             item.pourcentage = 100 * item.Debit / fTotal;
+    //         } );    
+    //         } catch (error) {
+                
+    //         }
+            
+		
+	// 	//console.log( JSON.stringify(ctx.instance.__data));
+	// 	next();
+	// });
 
 };
---
+++ server/boot/repartition.js
@@ -0,0 +1,20 @@
+module.exports = function (app) {
+    app.dataSources.sqlite3.connector.observe('after execute', function (ctx, next) {
+        try {
+            if (ctx.req.sql.indexOf("RepartitionAff") !== -1) {
+                var newRespone = ctx.res // Add your changes here.
+                var fTotal = ctx.res.reduce(function (sum, item) {
+                      return sum + item.Debit;
+                }, 0);
+                ctx.res.forEach(function (item) {
+                    item.pourcentage = 100 * item.Debit / fTotal;
+                });
+            }
+        } catch (error) {
+
+        }
+
+        next();
+        //   ctx.end(err, ctx, response);
+    });
+};
